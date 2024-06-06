c4bf11673f650593446b42c8e647450be2fa6d10
Bhoja <135154482+BhojaByte@users.noreply.github.com>
2024-06-07 02:47:59 +0500

    Add files via upload

---
+++ 2. Select statement and where.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics
+WHERE first_name = "leslie"
+;
+
+SELECT *
+FROM parks_and_recreation.employee_salary
+where salary <= 50000
+;
+
+select * 
+from parks_and_recreation.employee_demographics
+where gender = "female";
+
